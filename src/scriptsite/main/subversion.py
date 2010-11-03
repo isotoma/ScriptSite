f67e349420583c54281f2731833e99f26191ff1f
Tom Wardill <tom@howrandom.net>
2010-11-03 15:31:32 +0000

    Can take credentials, make file, make test run

--- src/scriptsite/main/subversion.py
+++ src/scriptsite/main/subversion.py
@@ -1,16 +1,27 @@
 import tempfile
 import os
+import shutil
 from datetime import datetime
 
 import pysvn
-from lxml import etree
+from lxml import etree 
+
+from django.core.files import File
 
 from scriptsite import settings
+from scriptsite.main.models import TestScript
+
+def make_script_model_from_file(script_file, flavour, revision):
+    """ Create a model object from the file that we have created """
+    ts = TestScript()
+    ts.flavour = flavour
+    ts.date_uploaded = datetime.now()
+    ts.script_file.save(flavour + '.xml', File(file(script_file)), save = False)
+    ts.revision = revision
+    return ts
 
 def get_output_directory(root_path):
-    creation_time = datetime.now().strftime('%y%m%d%H%M%S')
-    initial_path = os.path.join(settings.MEDIA_ROOT, 'uploaded_scripts')
-    final_path = os.path.join(initial_path, creation_time)
+    final_path = os.path.join(root_path, 'processed')
     return final_path
 
 def make_script_file_from_checkout(root_path, flavour):
@@ -63,6 +74,9 @@ def make_script_file_from_checkout(root_path, flavour):
     final_test_doc.attrib['generated'] = file_generation_time
     final_test_doc.append(config_tree.find('script_version'))
     
+    
+    # we are done here, write it to the same directory as the uploaded ones,
+    # and return
     output_dir = get_output_directory(root_path)
     os.makedirs(output_dir)
     output_file = open(os.path.join(output_dir, flavour + ".xml"), 'w')
@@ -78,7 +92,8 @@ def get_from_subversion(repo_url, revision, username, password, flavour):
     svn_password = password
 
     # make a directory to checkout to
-    export_dir = os.path.join(tempfile.mkdtemp(), 'export')
+    temp_dir = tempfile.mkdtemp()
+    export_dir = os.path.join(temp_dir, 'export')
     print export_dir
     
     # Get a checkout
@@ -96,6 +111,16 @@ def get_from_subversion(repo_url, revision, username, password, flavour):
         client.set_default_username("")
         client.set_default_password("")
     
-    # so, we've got stuff from subversion
-    # we should probably do something with it, no?
-    script_file = make_script_file_from_checkout(export_dir, flavour)
+    try:
+        # so, we've got stuff from subversion
+        # we should probably do something with it, no?
+        script_file = make_script_file_from_checkout(export_dir, flavour)
+        # now we have the file, make a db model
+        ts = make_script_model_from_file(script_file, flavour, "0")
+        ts.save()
+    finally:
+        # tidy up
+        shutil.rmtree(temp_dir)
+        
+    # aand we're done, let's get out of here
+    return ts
