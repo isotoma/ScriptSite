82a7b728ce4e294c1ac9e74aa7348bde376c2cea
Tom Wardill <tom@howrandom.net>
2011-03-05 08:57:53 +0000

    Can install as an egg, using isotoma.recipe.django v3

--- src/setup.py
+++ src/setup.py
@@ -1,11 +1,12 @@
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='scriptsite',
     version='0.0.1',
-    packages=[],
+    packages=find_packages(exclude=['ez_setup']),
     zip_safe=False,
     include_package_data=True,
+
 )
 
