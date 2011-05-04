dcf397f08ea35a13316716fb563d7d3663d2d857
Tom Wardill <tom@howrandom.net>
2011-05-04 08:37:40 +0100

    Change to postgres db settings

--- src/scriptsite/settings.py
+++ src/scriptsite/settings.py
@@ -23,10 +23,10 @@ PATH_SITE_ROOT = os.path.normpath(os.path.dirname(__file__))
 
 DATABASES = {
     'default': {
-        'ENGINE': 'sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': os.path.join(PATH_SITE_ROOT, 'site.db'),                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
+        'ENGINE': 'postgresql_psycopg2', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'scriptsite',                      # Or path to database file if using sqlite3.
+        'USER': 'scriptsite',                      # Not used with sqlite3.
+        'PASSWORD': 'scriptsite',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
